3ac106a658ca42d81da9402251e2cb6e3b5a7ab6
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-02-28 03:18:58 +0530

    React boilerplate, Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "extends": "airbnb",
+    "env": {
+        "browser": true,
+        "jest": true,
+    },
+    "rules": {
+        "react/jsx-filename-extension": 0
+    }
+};
